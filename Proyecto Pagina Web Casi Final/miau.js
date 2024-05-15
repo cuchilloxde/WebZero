c08c32baa8f15bc54b006f5f6ae447b4b1b50980
cuchilloxde <101745496+cuchilloxde@users.noreply.github.com>
2024-05-15 19:03:53 -0400

    Funciona la validacion faltan detalles pero lo basico funciona.

--- Proyecto Pagina Web Casi Final/miau.js
+++ Proyecto Pagina Web Casi Final/miau.js
@@ -254,14 +254,66 @@ formulario.addEventListener('submit', (e) => {
 
 
 //FORMULARIO CONTACTO
-const formulario = document.getElementById('formulario_contacto');
-const inputs = document.querySelectorAll('#form-control input');
+const form = document.getElementById('needs-validation');
+const nombre = document.getElementById('Nombre');
+const apellido = document.getElementById('Apellido');
+const email = document.getElementById('Email');
 
 const expresiones = {
     nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
     apellido: /^[a-zA-ZÀ-ÿ\s]{1,40}$/,
-    correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
+    email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
 
+}
+
+form.addEventListener('submit', e => {
+	e.preventDefault();
+	
+	checkInputs();
+});
+
+function checkInputs() {
+	
+	const nombreValue = nombre.value.trim();
+	const apellidodValue = apellido.value.trim();
+    const emailValue = email.value.trim();
+
+    //AHORA VALIDAMOS
+    if(nombreValue === '') {
+		setErrorFor(nombre, 'Debe escribir su nombre.');
+	} else {
+		setSuccessFor(nombre);
+	}
+
+    if(apellidoValue === '') {
+		setErrorFor(apellido, 'Debe escribir su apellido.');
+	} else {
+		setSuccessFor(apellido);
+	}
+
+    if(emailValue === '') {
+		setErrorFor(email, 'No puede dejar el email en blanco');
+	} else if (!isEmail(emailValue)) {
+		setErrorFor(email, 'No ingreso un email válido');
+	} else {
+		setSuccessFor(email);
+	}
+}
+
+function setErrorFor(input, mensaje) {
+	const formControl = input.parentElement;
+	const small = formControl.querySelector('small');
+	formControl.className = 'form-control error';
+	small.innerText = mensaje;
+}
+
+function setSuccessFor(input) {
+	const formControl = input.parentElement;
+	formControl.className = 'form-control success';
+}
+
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
 
 const campos ={
